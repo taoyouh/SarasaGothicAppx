f7ec0b20e2a47e93a20bb4a3bb5fe8921d922e08
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2021-04-21 14:52:45 +0800

    Update Font Version to 0.30.2

--- SarasaGothic/DownloadContents.ps1
+++ SarasaGothic/DownloadContents.ps1
@@ -3,9 +3,9 @@ if (-not $(Test-Path ./Temp))
     New-Item -ItemType Directory ./Temp
 }
 
-if (-not $(Test-Path ./Temp/sarasa-gothic-ttc-0.12.10.7z))
+if (-not $(Test-Path ./Temp/sarasa-gothic-ttc-0.30.2.7z))
 {
-    Invoke-WebRequest https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.12.10/sarasa-gothic-ttc-0.12.10.7z -OutFile ./Temp/sarasa-gothic-ttc-0.12.10.7z
+    Invoke-WebRequest https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.30.2/sarasa-gothic-ttc-0.30.2.7z -OutFile ./Temp/sarasa-gothic-ttc-0.30.2.7z
 }
 
 if (-not $(Test-Path ./Assets/Fonts))
@@ -13,4 +13,4 @@ if (-not $(Test-Path ./Assets/Fonts))
     New-Item -ItemType Directory ./Assets/Fonts
 }
 
-7z x ./Temp/sarasa-gothic-ttc-0.12.10.7z "-o./Assets/Fonts"
+7z x ./Temp/sarasa-gothic-ttc-0.30.2.7z "-o./Assets/Fonts"
