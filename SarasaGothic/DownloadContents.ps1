ebf7b5e775839d32971527751cacfc93a688fd7b
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2021-05-03 11:14:18 +0800

    Fix PowerShell Script

--- SarasaGothic/DownloadContents.ps1
+++ SarasaGothic/DownloadContents.ps1
@@ -5,9 +5,9 @@ if (-not $(Test-Path ./Temp))
     New-Item -ItemType Directory ./Temp
 }
 
-if (-not $(Test-Path ./Temp/sarasa-gothic-ttc-0.31.1.7z))
+if (-not $(Test-Path ./Temp/sarasa-gothic-ttc-${version}.7z))
 {
-    Invoke-WebRequest https://github.com/be5invis/Sarasa-Gothic/releases/download/v$(version)/sarasa-gothic-ttc-$(version).7z -OutFile ./Temp/sarasa-gothic-ttc-$(version).7z
+    Invoke-WebRequest https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/sarasa-gothic-ttc-${version}.7z -OutFile ./Temp/sarasa-gothic-ttc-${version}.7z
 }
 
 if (-not $(Test-Path ./Assets/Fonts))
@@ -15,4 +15,4 @@ if (-not $(Test-Path ./Assets/Fonts))
     New-Item -ItemType Directory ./Assets/Fonts
 }
 
-7z x ./Temp/sarasa-gothic-ttc-$(version).7z "-o./Assets/Fonts"
+7z x ./Temp/sarasa-gothic-ttc-${version}.7z "-o./Assets/Fonts"
