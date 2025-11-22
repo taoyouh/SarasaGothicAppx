13adda41046c6976bb464cf39589b40344df90f3
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2025-11-22 22:59:13 +0800

    Fix publish script and artifact collection error

--- Scripts/Publish.ps1
+++ Scripts/Publish.ps1
@@ -3,7 +3,7 @@ $clientId = "46a5301d-5df3-488d-9c8c-7b8d2732e3ab"
 $appxPath = Join-Path $PSScriptRoot "../SarasaGothic/AppPackages" -Resolve
 $appId = "9MW0M424NCZ7"
 
-$submissionPackagePath = Join-Path $PSScriptRoot "../StoreBroker/StorePackage" -Resolve
+$submissionPackagePath = Join-Path $PSScriptRoot "../StoreBroker/StorePackage"
 $submissionPackageName = "Submission"
 $clientSecret = $env:PUBLISH_CLIENTSECRET | ConvertTo-SecureString -AsPlainText -Force
 $configPath = Join-Path $PSScriptRoot "../StoreBroker/SBConfig.json" -Resolve
@@ -22,13 +22,13 @@ $cred = New-Object System.Management.Automation.PSCredential $clientId, $clientS
 Set-StoreBrokerAuthentication -TenantId $tenantId -Credential $cred
 
 Write-Host ("Looking for appxupload at " + $appxPath) -ForegroundColor Cyan
-$appxuploads = (Get-ChildItem -Path $appxPath | Where-Object (Name -like "*.appxupload") -or (Name -like "*.msixupload"))
+$appxuploads = Get-ChildItem -Path $appxPath | Where-Object { ".appxupload", ".msixupload" -contains $_.Extension }
 
 Write-Host "Creating submission package:" -ForegroundColor Cyan
 New-SubmissionPackage -ConfigPath $configPath -PDPRootPath $pdpRootPath -ImagesRootPath $imageRootPath -OutPath $submissionPackagePath -OutName $submissionPackageName -AppxPath $appxuploads.FullName
 
 Write-Host "Submitting package to Dev Center" -ForegroundColor Cyan
-Update-ApplicationSubmission -AppId $appId -SubmissionDataPath (Join-Path $submissionPackageName "${submissionPackageName}.json") -PackagePath (Join-Path $outPath ($submissionPackageName + ".zip")) -Force -ReplacePackages -UpdateListings -TargetPublishMode Manual -AutoCommit
+Update-ApplicationSubmission -AppId $appId -SubmissionDataPath (Join-Path $submissionPackagePath "${submissionPackageName}.json" -Resolve) -PackagePath (Join-Path $submissionPackagePath ($submissionPackageName + ".zip")) -Force -ReplacePackages -UpdateListings -TargetPublishMode Manual -AutoCommit
 
 Write-Host "Clearing Authentication" -ForegroundColor Cyan
 Clear-StoreBrokerAuthentication
